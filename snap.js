235af1c449d47dfbddfcc26b347d7bdaf8071a76
Artur Sabirov <mail@artursabirov.ru>
2016-10-31 09:54:33 +0400

    add displace option instead of dissableTranslate

--- snap.js
+++ snap.js
@@ -29,7 +29,7 @@
             touchToDrag: true,
             slideIntent: 40, // degrees
             minDragDistance: 5,
-            disableTranslate: false
+            displace: false
         },
         cache = {
             simpleStates: {
@@ -95,10 +95,6 @@
                 return (cache.vendor==='Moz' || cache.vendor==='ms') ? 'transitionend' : cache.vendor+'TransitionEnd';
             },
             canTransform: function(){
-                if (settings.disableTranslate) {
-                    return false;
-                }
-
                 return typeof settings.element.style[cache.vendor+'Transform'] !== 'undefined';
             },
             deepExtend: function(destination, source) {
@@ -168,7 +164,7 @@
                 get: {
                     matrix: function(index) {
 
-                        if( !utils.canTransform() ){
+                        if( !utils.canTransform() || settings.displace ){
                             return parseInt(settings.element.style.left, 10);
                         } else {
                             var matrix = win.getComputedStyle(settings.element)[cache.vendor+'Transform'].match(/\((.*)\)/),
@@ -200,7 +196,7 @@
                 },
                 easeTo: function(n) {
 
-                    if( !utils.canTransform() ){
+                    if( !utils.canTransform() || settings.displace ){
                         cache.translation = n;
                         action.translate.x(n);
                     } else {
@@ -212,7 +208,7 @@
                         cache.animatingInterval = setInterval(function() {
                             utils.dispatchEvent('animating');
                         }, 1);
-                        
+
                         utils.events.addEvent(settings.element, utils.transitionCallback(), action.translate.easeCallback);
                         action.translate.x(n);
                     }
@@ -224,7 +220,7 @@
                     if( (settings.disable==='left' && n>0) ||
                         (settings.disable==='right' && n<0)
                     ){ return; }
-                    
+
                     if( !settings.hyperextensible ){
                         if( n===settings.maxPosition || n>settings.maxPosition ){
                             n=settings.maxPosition;
@@ -232,13 +228,16 @@
                             n=settings.minPosition;
                         }
                     }
-                    
+
                     n = parseInt(n, 10);
                     if(isNaN(n)){
                         n = 0;
                     }
 
-                    if( utils.canTransform() ){
+                    if( settings.displace ){
+                        settings.element.style.left = n+'px';
+                        settings.element.style.right = '';
+                    } else if( utils.canTransform() ){
                         var theTranslate = 'translate3d(' + n + 'px, 0,0)';
                         settings.element.style[cache.vendor+'Transform'] = theTranslate;
                     } else {
@@ -266,25 +265,25 @@
                     // No drag on ignored elements
                     var target = e.target ? e.target : e.srcElement,
                         ignoreParent = utils.parentUntil(target, 'data-snap-ignore');
-                    
+
                     if (ignoreParent) {
                         utils.dispatchEvent('ignore');
                         return;
                     }
-                    
-                    
+
+
                     if(settings.dragger){
                         var dragParent = utils.parentUntil(target, settings.dragger);
-                        
+
                         // Only use dragger if we're in a closed state
-                        if( !dragParent && 
-                            (cache.translation !== settings.minPosition && 
+                        if( !dragParent &&
+                            (cache.translation !== settings.minPosition &&
                             cache.translation !== settings.maxPosition
                         )){
                             return;
                         }
                     }
-                    
+
                     utils.dispatchEvent('start');
                     settings.element.style[cache.vendor+'Transition'] = '';
                     cache.isDragging = true;
